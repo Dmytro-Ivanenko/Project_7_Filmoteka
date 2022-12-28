cf44ed01abf3d22b714487c5b810408f15293f7e
Dmytro-Ivanenko <dmitro0399@gmail.com>
2022-12-28 20:47:50 +0200

    update pagination stiles

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,5 +1,5 @@
 import Pagination from 'tui-pagination';
-import 'tui-pagination/dist/tui-pagination.css';
+// import 'tui-pagination/dist/tui-pagination.css';
 
 export function addPagination({ total_results }) {
   const options = {
