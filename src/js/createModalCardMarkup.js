8df7ee38aa7d95305eb72fab08c6799fef042d55
Dmytro-Ivanenko <dmitro0399@gmail.com>
2022-12-28 19:04:42 +0200

    add Budankov changes
    Add modal card, favicon

--- src/js/createModalCardMarkup.js
+++ src/js/createModalCardMarkup.js
@@ -20,10 +20,17 @@ export function createModalCardMarkup(data) {
     })
     .join(', ');
 
-  const instance = basicLightbox.create(`
+  return `
     <div class="modal">
-        <button class="modal-btn-close">X</button>
         <div class="modal-card">
+            <button class="modal__btn-close" data-modal-close>
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+            class="modal__icon-close"
+            width="24" height="24"
+            viewBox="0 0 24 24">
+            <path d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z"></path>
+            </svg>
+            </button>
             <div class="modal-card__images">
                 <img
                 class="modal-card__image"
@@ -62,6 +69,5 @@ export function createModalCardMarkup(data) {
             </div>
         </div>
     </div>
-`);
-  return instance;
+`;
 }
--- src/js/onCardClick.js
+++ src/js/onCardClick.js
@@ -1,14 +1,44 @@
 import { fetchApi } from '../index.js';
 import { createModalCardMarkup } from './createModalCardMarkup';
 
+const backdrop = document.querySelector('.backdrop');
+const modal = document.querySelector('[data-modal]');
+let closeModalBtn = document.querySelector('[data-modal-close]');
+
+modal.addEventListener('click', modalListener);
+
 export async function onCardClick(e) {
   if (e.path[2].className !== 'photo-card') {
     return;
   }
 
   if (e.path[2].className === 'photo-card') {
-    const id = e.path[2].dataset.id;
+    modal.classList.remove('is-hidden');
+    let id = e.path[2].dataset.id;
     const { data } = await fetchApi.getFilmToId(id);
-    createModalCardMarkup(data).show();
+    backdropBackground(data);
+    modal.insertAdjacentHTML('beforeend', createModalCardMarkup(data));
   }
+}
+
+function modalListener(e) {
+  if (
+    e.path[0].className === 'modal__btn-close' ||
+    e.target.classList[0] === 'modal__icon-close' ||
+    e.path[0].className === 'backdrop'
+  ) {
+    closeModalBtn = modal.classList.add('is-hidden');
+  }
+}
+
+export function closeModalEcs(e) {
+  if (e.code === 'Escape') {
+    closeModalBtn = modal.classList.add('is-hidden');
+  }
+}
+
+function backdropBackground(data) {
+  backdrop.style.backgroundImage = `url("https://image.tmdb.org/t/p/original/${data.backdrop_path}`;
+  backdrop.style.backgroundSize = 'cover';
+  backdrop.style.backgroundPosition = '50% 50%';
 }
