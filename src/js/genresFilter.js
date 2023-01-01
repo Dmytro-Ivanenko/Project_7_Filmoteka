c2f3129d12e67366da29d21a5bab7650ccbdeba7
TetyanaVynnyk <kharchenko1988@bigmir.net>
2023-01-01 20:09:47 +0200

    All murkup

--- src/js/genresFilter.js
+++ src/js/genresFilter.js
@@ -1,54 +1,52 @@
-// import Choices from 'choices.js';
-// import '../../node_modules/choices.js/public/assets/styles/choices.min.css';
+import { fetchApi } from '../index.js';
 
 export async function createGenresFilter() {
   const choiceBtn = document.querySelector('.choose-genre-btn');
+  const choiseFormWrapper = document.querySelector('.choice-form-wrapper');
+  const choiceFormWrapperElement = document.querySelector('.choice-form-wrapper__element');
+  const chooseBtn = document.querySelector('.chooseBtn');
+
   choiceBtn.addEventListener('click', openChoiceForm);
-  const choiseForm = document.querySelector('.choose-genre');
+  
 
-  function createChoiceForm() {
-    return `
-    <div class="choice-form-wrapper">
-        <form class = "choice-form-wrapper__element" >
-        <ul class="choice-form">
-        <li class="choice-form__item">
-            <input
-            type="checkbox"
-            id="genreType"
-            name="genreName"
-             />
-          <label for="genreType">GENRE TYPE</label>
-        </li>
-        <li>
-            <input
-            type="checkbox"
-            id="genreType"
-            name="genreName"
-             />
-          <label for="genreType">GENRE TYPE</label>
-        </li>
-        </ul>
-        </form>
-        <div class="choice-form-wrapper__btn">
-          <button class="chooseBtn" type="submit">CHOOSE</button>
-        </div>
-    </div>`;
+  function createChoiceElement({name}) {
+    return `<div> <input class="choice-form__item"
+    type="checkbox"
+    id="genreType"
+    name="genreName"
+     />
+    <label for="genreType">${name}</label>
+    </div>`
   }
 
+  
   function openChoiceForm() {
-    const choiseFormWrapper = document.querySelector('.choice-form-wrapper');
+    clearFormMurkup();
 
-    if (choiseFormWrapper) {
-      choiseFormWrapper.classList.remove('visually-hidden');
-    } else {
-      choiseForm.insertAdjacentHTML('beforeend', createChoiceForm());
-    };
-    const chooseBtn = document.querySelector('.chooseBtn')
+    choiseFormWrapper.classList.remove('visually-hidden');
+
     chooseBtn.addEventListener('click', searchFilmsToGenres);
+    
+    createFormMarkup();
   }
 
-  function searchFilmsToGenres(){
-    const choiseFormWrapper = document.querySelector('.choice-form-wrapper');
+  function searchFilmsToGenres() {
+    createFormMarkup();
     choiseFormWrapper.classList.add('visually-hidden');
+  }
+
+  const getGenreName = async () => {
+    const genreNames = await fetchApi.fillGenreList();
+    return genreNames;
+  };
+
+  const createFormMarkup = async () => {
+    const genreNames = await getGenreName();
+    const markup = genreNames.map(createChoiceElement);
+    choiceFormWrapperElement.insertAdjacentHTML('beforeend', markup.join(''));
+  }
+
+  function clearFormMurkup() {
+    choiceFormWrapperElement.innerHTML = '';
   }
 }
