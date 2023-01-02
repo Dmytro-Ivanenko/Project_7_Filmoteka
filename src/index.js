212d7365a3a4c162c61ab43ca92a2972cd0072c2
irnikshu <95768966+irnikshu@users.noreply.github.com>
2023-01-02 22:45:10 +0200

    add modal card of students v2

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { searchFilms } from './js/searchFilms';
 import { onCardClick } from './js/onCardClick';
 import { getTrailerFilm } from './js/getTrailerFilm';
 import { backToTop } from './js/backToTop';
+import {openModal, closeModal, onClick} from './js/createModalCardsStudents';
 
 // Variables
 const debounce = require('lodash.debounce');
@@ -17,6 +18,9 @@ export const body = document.querySelector('body');
 export const searchForm = document.querySelector('.search-form-input');
 export const galleryList = document.querySelector('.gallery');
 export const searchResult = document.querySelector('.search-result');
+export const modal = document.querySelector('#myModal');
+export const btn = document.querySelector('#myBtn');
+export const span = document.querySelector('.close');
 
 // Classes
 export const fetchApi = new FetchAPI();
@@ -27,3 +31,6 @@ document.addEventListener('DOMContentLoaded', renderTrendingFilms());
 document.addEventListener('DOMContentLoaded', createGenresFilter());
 document.addEventListener('click', getTrailerFilm);
 searchForm.addEventListener('input', debounce(searchFilms, DEBOUNCE_DELAY));
+btn.addEventListener('click', openModal);
+span.addEventListener('click', closeModal);
+window.addEventListener('click', onClick);
