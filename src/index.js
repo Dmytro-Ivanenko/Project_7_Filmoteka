a6b5876b9bb3efd587d621370068e3c67ca5c8a4
irnikshu <95768966+irnikshu@users.noreply.github.com>
2023-01-02 21:38:30 +0200

    add modal card of student

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import { searchFilms } from './js/searchFilms';
 import { onCardClick } from './js/onCardClick';
 import { getTrailerFilm } from './js/getTrailerFilm';
 import { backToTop } from './js/backToTop';
-import { onClickCardStudents } from './js/onClickCardStudents';
+import { openModal } from './js/createModalCardsStudents';
+import { closeModal } from './js/createModalCardsStudents';
+import { onClick } from './js/createModalCardsStudents';
 
 
 
@@ -18,7 +20,10 @@ const DEBOUNCE_DELAY = 300;
 export const searchForm = document.querySelector('.search-form-input');
 export const galleryList = document.querySelector('.gallery');
 export const searchResult = document.querySelector('.search-result');
-export const studentsModalCard = document.querySelector('.students-list');
+// export const studentsModalCard = document.querySelector('.students-list');
+export const modal = document.querySelector('#myModal');
+export const btn = document.querySelector('#myBtn');
+export const span = document.querySelector('.close');
 
 
 // Classes
@@ -29,4 +34,10 @@ document.addEventListener('click', onCardClick);
 document.addEventListener('DOMContentLoaded', renderTrendingFilms());
 document.addEventListener('click', getTrailerFilm);
 searchForm.addEventListener('input', debounce(searchFilms, DEBOUNCE_DELAY));
-studentsModalCard.addEventListener('click', onClickCardStudents);
+// studentsModalCard.addEventListener('click', onClickCardStudents);
+btn.addEventListener('click', openModal);
+span.addEventListener('click', closeModal);
+window.addEventListener('click', onClick);
+
+
+
--- src/js/createModalCardsStudents.js
+++ src/js/createModalCardsStudents.js
@@ -1,6 +1,6 @@
-import { galleryStudents } from './galleryStudents.js';
+// import { galleryStudents } from './galleryStudents.js';
 
-console.log(galleryStudents);
+// console.log(galleryStudents);
 
 // export function createModalCardsStudents(galleryStudents) {
 //     return galleryStudents
@@ -24,27 +24,49 @@ console.log(galleryStudents);
    
 // }
 
-export function createModalCardsStudents(galleryStudents) {
-    const {
-        photo,
-        name,
-        profession,
-        email,
-        GitHub
-    } = galleryStudents;
+// export function createModalCardsStudents(galleryStudents) {
+//     const {
+//         photo,
+//         name,
+//         profession,
+//         email,
+//         GitHub
+//     } = galleryStudents;
 
-    return `<div class="modalcard">
+//     return `<div class="modalcard">
   
-    <img class="modalcard__image"
-      src="${photo}"
-      alt="${name}"
-    />
-    <div class="modalcard__description">
-<p class="modalcard__name">${name}</p>
-<p class="modalcard__name">${profession}</p>
-<p class="modalcard__email">${email}</p>
-<p class="modalcard__github">${GitHub}</p>
-    </div>
+//     <img class="modalcard__image"
+//       src="${photo}"
+//       alt="${name}"
+//     />
+//     <div class="modalcard__description">
+// <p class="modalcard__name">${name}</p>
+// <p class="modalcard__name">${profession}</p>
+// <p class="modalcard__email">${email}</p>
+// <p class="modalcard__github">${GitHub}</p>
+//     </div>
   
-</div>`;
+// </div>`;
+// }
+
+// const modal = document.querySelector('#myModal');
+// const btn = document.querySelector('#myBtn');
+// const span = document.querySelector('.close');
+
+// btn.addEventListener('click', openModal)
+
+export function openModal() {
+   modal.style.display = "block";
+}
+// span.addEventListener('click', closeModal)
+
+export function closeModal() {
+   modal.style.display = "none";
+}
+// window.addEventListener('click', onClick)
+
+export function onClick(event) {
+   if (event.target === modal) {
+    modal.style.display = "none";
+  }
 }
