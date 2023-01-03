d0f5dbfd8186fc0a9b5f22872448d3d1047382d9
Alyona-Nekhryshchyniuk <o.nekhryshchyniuk@gmail.com>
2023-01-03 21:23:24 +0200

    swiper for students cards import

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import { searchFilms } from './js/searchFilms';
 import { onCardClick } from './js/onCardClick';
 import { getTrailerFilm } from './js/getTrailerFilm';
 import { backToTop } from './js/backToTop';
-import { toggleModal, onClick } from './js/createModalCardsStudents';
+import {
+  toggleModal,
+  closeModalOnBackdropClick,
+} from './js/createModalCardsStudents';
 
 // Variables
 const debounce = require('lodash.debounce');
@@ -17,9 +20,8 @@ export const body = document.querySelector('body');
 export const searchForm = document.querySelector('.search-form-input');
 export const galleryList = document.querySelector('.gallery');
 export const searchResult = document.querySelector('.search-result');
-export const modal = document.querySelector('#myModal');
-export const btn = document.querySelector('#myBtn');
-export const span = document.querySelector('.close');
+const studentsBtn = document.querySelector('.students-button');
+const closeBtn = document.querySelector('.close-button');
 
 // Classes
 export const fetchApi = new FetchAPI();
@@ -30,6 +32,6 @@ document.addEventListener('DOMContentLoaded', renderTrendingFilms());
 // document.addEventListener('DOMContentLoaded', createGenresFilter());
 document.addEventListener('click', getTrailerFilm);
 searchForm.addEventListener('input', debounce(searchFilms, DEBOUNCE_DELAY));
-btn.addEventListener('click', toggleModal);
-span.addEventListener('click', toggleModal);
-window.addEventListener('click', onClick);
+studentsBtn.addEventListener('click', toggleModal);
+closeBtn.addEventListener('click', toggleModal);
+window.addEventListener('click', closeModalOnBackdropClick);
--- src/js/createModalCardsStudents.js
+++ src/js/createModalCardsStudents.js
@@ -1,10 +1,29 @@
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.min.css';
+
 const backdrop = document.querySelector('.students-backdrop');
 
 export function toggleModal() {
   backdrop.classList.toggle('is-hidden');
+
+  if (!backdrop.classList.contains('is-hidden')) {
+    new Swiper('.swiper', {
+      speed: 400,
+      spaceBetween: 100,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+      },
+    });
+  }
 }
 
-export function onClick(event) {
+export function closeModalOnBackdropClick(event) {
   if (event.target === backdrop) {
     backdrop.classList.add('is-hidden');
   }
