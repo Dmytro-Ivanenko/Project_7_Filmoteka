bda39ef75a34695719752fb5fc9ecd461b9dafe4
Alyona-Nekhryshchyniuk <o.nekhryshchyniuk@gmail.com>
2023-01-04 21:06:27 +0200

    students scss update

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 //Imports
 // import './js/auth';
 // import './js/addToLibrary';
-
 import { FetchAPI } from './js/api';
 import { renderTrendingFilms } from './js/renderTrendingFilms';
 import { searchFilms } from './js/searchFilms';
 import { onCardClick } from './js/onCardClick';
 import { getTrailerFilm } from './js/getTrailerFilm';
 import { backToTop } from './js/backToTop';
-
-
 import {
   toggleModal,
   closeModalOnBackdropClick,
 } from './js/createModalCardsStudents';
-
 
 // Variables
 const debounce = require('lodash.debounce');
@@ -24,10 +20,8 @@ export const body = document.querySelector('body');
 export const searchForm = document.querySelector('.search-form-input');
 export const galleryList = document.querySelector('.gallery');
 export const searchResult = document.querySelector('.search-result');
-
 const studentsBtn = document.querySelector('.students-button');
 const closeBtn = document.querySelector('.close-button');
-
 
 // Classes
 export const fetchApi = new FetchAPI();
@@ -38,8 +32,6 @@ document.addEventListener('DOMContentLoaded', renderTrendingFilms());
 // document.addEventListener('DOMContentLoaded', createGenresFilter());
 document.addEventListener('click', getTrailerFilm);
 searchForm.addEventListener('input', debounce(searchFilms, DEBOUNCE_DELAY));
-
 studentsBtn.addEventListener('click', toggleModal);
 closeBtn.addEventListener('click', toggleModal);
 window.addEventListener('click', closeModalOnBackdropClick);
-
