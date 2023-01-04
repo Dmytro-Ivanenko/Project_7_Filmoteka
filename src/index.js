354630d18bc07e26400e5669f166042a24ffefdb
Dmytro-Ivanenko <dmitro0399@gmail.com>
2023-01-04 12:50:01 +0200

    update imports

--- src/index.js
+++ src/index.js
@@ -1,45 +1,26 @@
-
 import { entryField } from './js/searchFilms';
 import { onCardClick } from './js/onCardClick';
 import { getTrailerFilm } from './js/getTrailerFilm';
 import { backToTop } from './js/backToTop';
 
 //Imports
-//import './js/auth';
-//import './js/addToLibrary';
-//import { FetchAPI } from './js/api';
-//import { renderTrendingFilms } from './js/renderTrendingFilms';
-//import { searchFilms } from './js/searchFilms';
-//import { onCardClick } from './js/onCardClick';
-//import { getTrailerFilm } from './js/getTrailerFilm';
-//import { backToTop } from './js/backToTop';
-//import {createGenresFilter} from './js/genresFilter';
-//import { switchMode } from './js/mode';
-//import { translate } from './js/translate';
-//import { fetchGenreUrl } from './js/api';
+import './js/auth';
+import './js/addToLibrary';
+import { FetchAPI } from './js/api';
 
+import { createGenresFilter } from './js/genresFilter';
+import { switchMode } from './js/mode';
 
+import { fetchGenreUrl } from './js/api';
 
-
-// Variables
-//const debounce = require('lodash.debounce');
-//const DEBOUNCE_DELAY = 300;
-
-//export const body = document.querySelector('body');
-//export const searchForm = document.querySelector('.search-form-input');
-//export const galleryList = document.querySelector('.gallery');
-//export const searchResult = document.querySelector('.search-result');
-//export const modeCheckbox = document.querySelector('.mode-checkbox');
+export const body = document.querySelector('body');
+export const galleryList = document.querySelector('.gallery');
+export const modeCheckbox = document.querySelector('.mode-checkbox');
 
 // Classes
-//export const fetchApi = new FetchAPI();
+export const fetchApi = new FetchAPI();
 
 // Listeners
-//document.addEventListener('click', onCardClick);
-//document.addEventListener('DOMContentLoaded', renderTrendingFilms());
-//document.addEventListener('DOMContentLoaded', createGenresFilter());
-//document.addEventListener('click', getTrailerFilm);
-//searchForm.addEventListener('input', debounce(searchFilms, DEBOUNCE_DELAY));
-//modeCheckbox.addEventListener('click', switchMode);
-
-
+// document.addEventListener('click', onCardClick);
+document.addEventListener('DOMContentLoaded', createGenresFilter);
+modeCheckbox.addEventListener('click', switchMode);
