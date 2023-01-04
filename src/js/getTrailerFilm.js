409cfa850ea5f4b5d6f8ddf4ed0c6fada9f30726
Dmytro-Ivanenko <107716921+Dmytro-Ivanenko@users.noreply.github.com>
2023-01-04 11:53:41 +0200

    Update getTrailerFilm.js

--- src/js/getTrailerFilm.js
+++ src/js/getTrailerFilm.js
@@ -10,7 +10,7 @@ export async function getTrailerFilm(e) {
   if (value !== 'gallery-item-trailer-icon') {
     return;
   }
-  movie = await refs.fetchApi.getTrailer(id);
+  const movie = await refs.fetchApi.getTrailer(id);
   const { key } = movie.data.results[0];
   console.log(key);
   //   yotubeMovie = getTrailerYouTube(key);
