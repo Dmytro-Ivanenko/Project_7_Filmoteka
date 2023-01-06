ca1cf6107862a3d771a41e56c0fb121ea628e4e0
Dmytro-Ivanenko <107716921+Dmytro-Ivanenko@users.noreply.github.com>
2023-01-06 11:35:06 +0200

    Update renderUpcoming.js

--- src/js/renderUpcoming.js
+++ src/js/renderUpcoming.js
@@ -1,23 +1,23 @@
-// import { createElementsUpcoming } from './createElementsUpcoming';
-// import { refs } from './refs';
-// import Glide from '@glidejs/glide';
+ import { createElementsUpcoming } from './createElementsUpcoming';
+ import { refs } from './refs';
+ import Glide from '@glidejs/glide';
 
-// export async function renderUpcoming() {
-//   refs.upcomingList.innerHTML = '';
-//   const { data } = await refs.fetchApi.fetchUpcoming();
+ export async function renderUpcoming() {
+   refs.upcomingList.innerHTML = '';
+   const { data } = await refs.fetchApi.fetchUpcoming();
 
-//   const upcomingEl = data.results;
+   const upcomingEl = data.results;
 
-//   const upcomingElements = upcomingEl.map(elem =>
-//     createElementsUpcoming(elem, refs.fetchApi)
-//   );
-//   refs.upcomingList.innerHTML = upcomingElements.join('');
+   const upcomingElements = upcomingEl.map(elem =>
+     createElementsUpcoming(elem, refs.fetchApi)
+   );
+   refs.upcomingList.innerHTML = upcomingElements.join('');
 
-//   var glide = new Glide('.glide', {
-//     type: 'carousel',
-//     startAt: 0,
-//     perView: 7,
-//     autoplay: 2500,
-//   });
-//   glide.mount();
-// }
+   var glide = new Glide('.glide', {
+     type: 'carousel',
+     startAt: 0,
+     perView: 7,
+     autoplay: 2500,
+   });
+   glide.mount();
+ }
