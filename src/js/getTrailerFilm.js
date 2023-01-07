d73b75995461eada7976f96d9c42ad399107380b
Marek Rożko <maknamar12@gmail.com>
2023-01-07 22:40:06 +0100

    add translate form

--- src/js/getTrailerFilm.js
+++ src/js/getTrailerFilm.js
@@ -13,8 +13,9 @@ async function getTrailerFilm(e) {
     renderModalTrailler(e.path[1].dataset.id);
 
     function renderModalTrailler(id) {
-      const API_KEY = '53f2c47317a563cd2628c68ceb6a6673';
-      const url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&language=en-US`;
+      if (window.location.hash === '#ua') {
+        const API_KEY = '53f2c47317a563cd2628c68ceb6a6673';
+      const url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&language=uk-UA`;
       fetch(url)
         .then(response => response.json())
         .then(data => {
@@ -33,6 +34,29 @@ async function getTrailerFilm(e) {
           instance.show();
           btnCloseModalTrailler(instance);
         });
+      } else {
+        const API_KEY = '53f2c47317a563cd2628c68ceb6a6673';
+      const url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&language=en-Us`;
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          const id = data.results[0].key;
+          const instance = basicLightbox.create(`
+  <iframe width="560" height="315" src='https://www.youtube.com/embed/${id}'frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+`);
+          instance.show();
+          btnCloseModalTrailler(instance);
+        })
+        .catch(() => {
+          const instance = basicLightbox.create(`
+    <iframe width="560" height="315" src='http://www.youtube.com/embed/RDuzj5VHxHi7M' frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      `);
+
+          instance.show();
+          btnCloseModalTrailler(instance);
+        });
+      }
+      
     }
 
     function btnCloseModalTrailler(instance) {
