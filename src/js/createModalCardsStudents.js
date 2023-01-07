1eed83332dbacf73e9ad35b8bbab1c619b294895
Alyona-Nekhryshchyniuk <o.nekhryshchyniuk@gmail.com>
2023-01-07 13:59:52 +0200

    students modal adaptive

--- src/js/createModalCardsStudents.js
+++ src/js/createModalCardsStudents.js
@@ -1,6 +1,7 @@
 import Swiper, { Navigation, Pagination } from 'swiper';
 import 'swiper/swiper-bundle.min.css';
 
+const body = document.querySelector('body');
 const backdrop = document.querySelector('.students-backdrop');
 const swiper = new Swiper('.swiper', {
   modules: [Navigation, Pagination],
@@ -8,35 +9,27 @@ const swiper = new Swiper('.swiper', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-    
   },
   speed: 1000,
-  // spaceBetween: 1,
-  
-  // slidesPerView: 3,
-
   
 
-  // slidesPerView: 1,
-  // spaceBetween: 100,
+  
   breakpoints: {
    // when window width is >= 320px
-    320: {
+    767: {
       slidesPerView: 1,
-      // spaceBetween: 2
     },
    // when window width is >= 768px
-   768: {
+   1023: {
       slidesPerView: 2,
-      // spaceBetween: 3
+
     },
      // when window width is >= 1280px
-    1280: {
+    1300: {
       slidesPerView: 3,
-      // spaceBetween: 4
     }
   },
- 
+
 
   pagination: {
     el: '.swiper-pagination',
