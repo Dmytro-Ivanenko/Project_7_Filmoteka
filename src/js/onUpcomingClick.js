0e846245169d1f6e9455dc3c42fee475d230951f
Dmytro-Ivanenko <107716921+Dmytro-Ivanenko@users.noreply.github.com>
2023-01-08 20:43:37 +0200

    Revert "Exports-and-listeners-refactoring"

--- src/js/onUpcomingClick.js
+++ src/js/onUpcomingClick.js
@@ -4,4 +4,7 @@ export const slides = document.querySelector('.glide__slides');
 
 slides.addEventListener('click', onUpcomingClick);
 
-
+// ============================== onUpcomingClick ==================================
+// Переїхала в модуль onCardClick бо там простіше працювати з
+// currentMovie і не треба робити купу експортів
+// =================================================================================
